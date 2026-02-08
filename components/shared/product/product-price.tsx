3f20b36955dfa5ad083c9a8922127afeb0e4e774
daddimohammed <mohadaddi618@gmail.com>
2026-02-08 14:09:04 +0100

    07-create-todays-deals-slider

--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -43,17 +43,14 @@ const ProductPrice = ({
         </span>
       </div>
       <div
-        className={`flex ${forListing && 'justify-center'} items-center gap-2`}
-      >
+        className={`flex ${forListing && 'justify-center'} items-center gap-2`}>
         <div className={cn('text-3xl', className)}>
           <span className='text-xs align-super'>$</span>
           {intValue}
           <span className='text-xs align-super'>{floatValue}</span>
         </div>
         <div className='text-muted-foreground text-xs py-2'>
-          Was:
-          <span className='line-through'>
-            {formatCurrency(listPrice)}
+          Was:<span className='line-through'>{formatCurrency(listPrice)}
           </span>
         </div>
       </div>
@@ -70,8 +67,7 @@ const ProductPrice = ({
       </div>
       <div className='text-muted-foreground text-xs py-2'>
         List price:{''}
-        <span className='line-through'>
-          {formatCurrency(listPrice)}
+        <span className='line-through'>{formatCurrency(listPrice)}
         </span>
       </div>
     </div>
