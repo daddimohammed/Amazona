3db0db3ced1d706f3adc480ef169197ee2b72664
daddimohammed <mohadaddi618@gmail.com>
2026-02-17 11:07:17 +0100

    15-register-user

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,13 +1,10 @@
-
-import Link from 'next/link'
-    import CartButton from './cart-button'
+import CartButton from './cart-button'
+import UserButton from './user-button'
 export default function Menu(){
     return(
         <div className='flex justify-end'>
         <nav className='flex gap-3 w-full'>
-            <Link href='/signin' className="header-button flex items-center">
-                Hello, Sign in
-            </Link>
+            <UserButton />
             <CartButton />
         </nav>
       </div>
